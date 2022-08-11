dbc5740938ede523530c02ffa41d957ba49180e8
Callum Loh <callumloh@gmail.com>
2022-08-11 11:47:43 +0100

    Add base apps that I need

---
+++ apps/nzbhydra2/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=SC1091
+test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
+test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
+
+#shellcheck disable=SC2086
+exec \
+    /usr/bin/python3 \
+        /app/nzbhydra2wrapperPy3.py \
+            --nobrowser \
+            --datafolder /config \
+            "$@"
