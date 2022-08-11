14ae8812ed1658fd15ac30090df2d7036849e0c3
Callum Loh <callumloh@gmail.com>
2022-08-11 13:43:02 +0100

    Cleanup entrypoint after some feedback

--- apps/mylar3/entrypoint.sh
+++ apps/mylar3/entrypoint.sh
@@ -2,17 +2,12 @@
 
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
-test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
-
-
-# Create the config dir if we need too
-mkdir -p /config
 
 #shellcheck disable=SC2086
 exec \
     /usr/bin/python3 \
         /app/Mylar.py \
         --nolaunch \
-        --port ${MYLAR3__PORT:-8090} \
+        --port ${MYLAR3__PORT} \
         --datadir /config \
         "$@"
