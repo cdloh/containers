713670fe0d22b2fe02588b5f43d9dd4fd39451e1
Callum Loh <callumloh@gmail.com>
2022-08-13 01:20:36 +0100

    Move NordVPN scripts to a directory that isn't used by the base

--- apps/nordvpn/client.sh
+++ apps/nordvpn/client.sh
@@ -4,7 +4,7 @@ test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
 sleep 30
 
-/scripts/login.sh
-/scripts/config.sh
-/scripts/connect.sh
-/scripts/watch.sh
+/nordvpn-scripts/login.sh
+/nordvpn-scripts/config.sh
+/nordvpn-scripts/connect.sh
+/nordvpn-scripts/watch.sh
