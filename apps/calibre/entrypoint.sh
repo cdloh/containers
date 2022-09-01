96e1a496a1aa9f2cb5d768f84e0906bab7db23cd
Callum Loh <callumloh@gmail.com>
2022-09-01 17:58:17 +0100

    Move to /app dir instead of /opt stick with current conventions (#21)

--- apps/calibre/entrypoint.sh
+++ apps/calibre/entrypoint.sh
@@ -5,12 +5,12 @@ test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
 if [[ "${CALIBRE__CREATE_LIBRARY}" == "true" && ! -f "${CALIBRE__LIBRARY}/metadata.db" ]]; then
     # Populate a blank library
-    /opt/calibre/calibredb --library-path="${CALIBRE__LIBRARY}" list
+    /app/calibredb --library-path="${CALIBRE__LIBRARY}" list
 fi
 
 #shellcheck disable=SC2086
 exec \
-    /opt/calibre/calibre-server \
+    /app/calibre-server \
         --port=${CALIBRE__PORT} \
         ${CALIBRE__LIBRARY} \
         "$@"
