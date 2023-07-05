6b320ce3f50b059b1b3186a0d4b3182174a55083
Callum Loh <callumloh@gmail.com>
2023-07-05 16:47:22 +0100

    Sync with onedr0p repo

--- apps/prowlarr/entrypoint.sh
+++ apps/prowlarr/entrypoint.sh
@@ -51,7 +51,7 @@ fi
 
 #shellcheck disable=SC2086
 exec \
-    /app/Prowlarr \
+    /app/bin/Prowlarr \
         --nobrowser \
         --data=/config \
         "$@"
--- apps/qbittorrent/ci/latest.sh
+++ apps/qbittorrent/ci/latest.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-# version=$(curl -sX GET "https://pkgs.alpinelinux.org/packages?name=qbittorrent-nox&branch=v3.17&arch" | grep -oP '(?<=<td class="version">)[^<]*')
-version=$(curl -sX GET "https://repology.org/api/v1/projects/?search=qbittorrent&inrepo=alpine_edge" | jq -r '.qbittorrent | .[] | select((.repo == "alpine_edge" and .binname == "qbittorrent-nox")) | .version' 2>/dev/null)
-version="${version%%_*}"
-version="${version%%-*}"
-printf "%s" "${version}"
+channel=$1
+
+if [[ "${channel}" == "stable" ]]; then
+    version=$(curl -sL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -r '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"' 2>/dev/null)
+    version="${version#*release-}"
+    version="${version%%_*}"
+    printf "%s" "${version}"
+fi
+
+if [[ "${channel}" == "beta" ]]; then
+    version=$(curl -sL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -r '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"' 2>/dev/null)
+    version="${version#*release-}"
+    version="${version%%_*}"
+    printf "%s" "${version}"
+fi
