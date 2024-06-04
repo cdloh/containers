42f162e1f278c274af2ca065aa5426ec3340bbb8
Callum Loh <callumloh@gmail.com>
2024-06-04 13:29:55 +0100

    Correct package for transmission

--- apps/transmission/ci/latest.sh
+++ apps/transmission/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# renovate: datasource=repology depName=alpine_3_20/dante-server versioning=loose
+# renovate: datasource=repology depName=alpine_3_20/transmission-daemon versioning=loose
 version="4.0.4-r2"
 printf "%s" "${version}"
