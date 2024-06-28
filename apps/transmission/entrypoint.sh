27f0e154dc7d3eac525fa62981f52e82be9fa57b
Callum Loh <callumloh@gmail.com>
2024-06-28 15:21:23 +0100

    Remove envsub as *arr apps dont need it anymore

--- apps/transmission/entrypoint.sh
+++ apps/transmission/entrypoint.sh
@@ -4,14 +4,6 @@
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 test -f "/scripts/vpn.sh" && source "/scripts/vpn.sh"
 
-# Update settings.json with environment variables
-envsubst < /app/settings.json.tmpl > /config/settings.json
-
-if [[ "${TRANSMISSION__DEBUG}" == "true" ]]; then
-    echo "Transmission starting with the following configuration..."
-    cat /config/settings.json
-fi
-
 #shellcheck disable=SC2086
 exec \
     /app/transmission-daemon \
