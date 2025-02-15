efea436f6ec927c7836b511aa1457104ed112e80
Callum Loh <callumloh@gmail.com>
2025-02-15 22:22:25 +0000

    Fix megatools

--- apps/megatools/ci/latest.sh
+++ apps/megatools/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git clone --quiet https://megous.com/git/megatools /tmp/megatools
+git clone --quiet https://xff.cz/git/megatools /tmp/megatools
 pushd /tmp/megatools > /dev/null || exit
 version=$(git log --date-order --tags --simplify-by-decoration --pretty=format:'%(describe).%cd' --date=format:"%Y%m%d" | head -n1)
 popd > /dev/null || exit
